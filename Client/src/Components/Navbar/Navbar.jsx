27e0a7c673290e79cc4f2763a1d8a8cc94048be2
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-04-06 10:12:25 -0300

    Update Navbar.jsx

--- Client/src/Components/Navbar/Navbar.jsx
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -11,11 +11,21 @@ import identidade from "../../images/icons navbar/IDENTIDADE.png";
 import avatar from "../../images/AVATAR-ICON.png";
 import exit from "../../images/EXIT.png";
 
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { logoutUser } from "../../Features/authSlice";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
-  let logado = 1;
+  const auth = useSelector((state) => {
+    return state.auth;
+  });
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const House = () => {
-
+    navigate("/home-login");
   }
 
   const User = () => {
@@ -23,7 +33,7 @@ const Navbar = () => {
   };
 
   const Dashboard = () => {
-
+    navigate("/dashboard");
   };
 
   const cartões = () => {
@@ -31,14 +41,23 @@ const Navbar = () => {
   };
 
   const Naoadmin = () => {
-
+    toast.error("Apenas Admin podem acessar");
   }
 
-  const logout = () => {
-
+  const admin = () => {
+    
   }
 
-  if (logado === 1) {
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    toast.warning("Logout com sucesso");
+
+    setTimeout(() => {
+      navigate("/");
+    }, "0");
+  };
+
+  if (auth.isAdmin) {
     return (
       <div className="container">
         <div className="container-image">
@@ -59,84 +78,6 @@ const Navbar = () => {
           </div>
 
           <div className="container-icons__quarto" onClick={cartões}>
-            <img src={cartao} alt="icon cartao navbar" />
-          </div>
-
-          <div className="container-icons__quinto" onClick={Naoadmin}>
-            <img src={identidade} alt="icon identidade navbar" />
-          </div>
-        </div>
-
-        <div className="container-user">
-          <div className="container-avatar">
-            <img src={avatar} alt="icon avatar navbar" />
-          </div>
-          <div className="container-exit" onClick={logout}>
-            <img src={exit} alt="icon exit navbar" />
-          </div>
-        </div>
-      </div>
-    );
-  } else if (logado === 0) {
-    return (
-      <div className="container">
-        <div className="container-image">
-          <img src={logo} alt="logo do solf1" />
-        </div>
-
-        <div className="container-icons">
-          <div className="container-icons__primeiro" onClick={House}>
-            <img src={house} alt="icon house navbar" />
-          </div>
-
-          <div className="container-icons__segundo" onClick={NaoUser}>
-            <img src={user} alt="icon user navbar" />
-          </div>
-
-          <div className="container-icons__terceiro" onClick={NaoDashboard}>
-            <img src={galeria} alt="icon galeria navbar" />
-          </div>
-
-          <div className="container-icons__quarto" onClick={Naocartões}>
-            <img src={cartao} alt="icon cartao navbar" />
-          </div>
-
-          <div className="container-icons__quinto" onClick={Naoadmin}>
-            <img src={identidade} alt="icon identidade navbar" />
-          </div>
-        </div>
-
-        <div className="container-user">
-          <div className="container-avatar">
-            <img src={avatar} alt="icon avatar navbar" />
-          </div>
-          <div className="container-exit" onClick={logout}>
-            <img src={exit} alt="icon exit navbar" />
-          </div>
-        </div>
-      </div>
-    );
-  } else if (logado === 2) {
-    return (
-      <div className="container">
-        <div className="container-image">
-          <img src={logo} alt="logo do solf1" />
-        </div>
-
-        <div className="container-icons">
-          <div className="container-icons__primeiro" onClick={House}>
-            <img src={house} alt="icon house navbar" />
-          </div>
-
-          <div className="container-icons__segundo" onClick={NaoUser}>
-            <img src={user} alt="icon user navbar" />
-          </div>
-
-          <div className="container-icons__terceiro" onClick={NaoDashboard}>
-            <img src={galeria} alt="icon galeria navbar" />
-          </div>
-
-          <div className="container-icons__quarto" onClick={Naocartões}>
             <img src={cartao} alt="icon cartao navbar" />
           </div>
 
@@ -167,15 +108,15 @@ const Navbar = () => {
             <img src={house} alt="icon house navbar" />
           </div>
 
-          <div className="container-icons__segundo" onClick={NaoUser}>
+          <div className="container-icons__segundo" onClick={User}>
             <img src={user} alt="icon user navbar" />
           </div>
 
-          <div className="container-icons__terceiro" onClick={NaoDashboard}>
+          <div className="container-icons__terceiro" onClick={Dashboard}>
             <img src={galeria} alt="icon galeria navbar" />
           </div>
 
-          <div className="container-icons__quarto" onClick={Naocartões}>
+          <div className="container-icons__quarto" onClick={cartões}>
             <img src={cartao} alt="icon cartao navbar" />
           </div>
 
@@ -186,9 +127,9 @@ const Navbar = () => {
 
         <div className="container-user">
           <div className="container-avatar">
-            <img src={avatar} alt="icon avatar navbar" />
+            <img src={`/upload/${auth.Img}`} alt="icon avatar navbar" />
           </div>
-          <div className="container-exit" onClick={logout}>
+          <div className="container-exit" onClick={handleLogout}>
             <img src={exit} alt="icon exit navbar" />
           </div>
         </div>
