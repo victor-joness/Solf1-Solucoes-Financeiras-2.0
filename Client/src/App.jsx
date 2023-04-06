4ee03cab175c9dea38e947a05cb98a072603650f
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-04-06 10:12:11 -0300

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,8 +5,15 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
+import Dashboard from './Pages/Dashboard/Dashboard';
+import AuthPage from './Pages/AuthPage/AuthPage';
+
+//rotas de login
+import HomeLogin from "./Pages/Dashboard/HomeLogin/HomeLogin";
+
 
 import {ToastContainer} from "react-toastify";
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return(
@@ -14,9 +21,14 @@ function App() {
       <BrowserRouter>
       <ToastContainer />
         <Routes>
+          <Route path='*' element={<NotFound/>}/>
           <Route path='/' element={<Home/>}/>
+          <Route path='/auth' element={<AuthPage/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
+
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/home-login' element={<HomeLogin/>}/>
         </Routes>
       </BrowserRouter>
     </div>
