c983874f9d27b50e67b7ee5e013416476354d79e
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-04-08 09:53:37 -0300

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const multer = require("multer");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -12,6 +14,23 @@ const register = require("./Routes/register");
 //endpoints
 app.use("/api/register", register);
 app.use("/api/login", login);
+
+//Multer Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  }
+})
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 const port = process.env.PORT || 5005;
 
