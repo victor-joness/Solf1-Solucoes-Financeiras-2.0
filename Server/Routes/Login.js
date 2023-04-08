f6ce85973102d2d3e1bac933d918ace79bd6b0e6
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-04-08 09:53:35 -0300

    Update Login.js

--- Server/Routes/Login.js
+++ Server/Routes/Login.js
@@ -13,43 +13,47 @@ const db = mysql.createConnection({
 });
 
 router.post("/", async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-  
-    db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
-      if (err) {
-        res.send(err);
-      }
+  const email = req.body.email;
+  const password = req.body.password;
 
-      if (result.length > 0) {
-        bcript.compare(password, result[0].password, (error, response) => {
-          if (error) {
-            res.send(error);
-          }
-          if (response) {
-            res.send({
-              msg: "Usuário logado!",
-              usuario: {
+  db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+
+    if (result.length > 0) {
+      bcript.compare(password, result[0].password, (error, response) => {
+        if (error) {
+          res.send(error);
+        }
+        if (response) {
+          res.send({
+            msg: "Usuário logado!",
+            usuario: {
+              id: result[0].id,
+              name: result[0].name,
+              email: result[0].email,
+              celular: result[0].celular,
+              endereco: result[0].endereco,
+              token: genAuthToken({
                 id: result[0].id,
                 name: result[0].name,
                 email: result[0].email,
-                token: genAuthToken({
-                  id: result[0].id,
-                  name: result[0].name,
-                  email: result[0].email,
-                  isAdmin: result[0].isAdmin,
-                  Img : result[0].Img,
-                }),
-              },
-            });
-          } else {
-            res.send({ msg: "Senha incorreta!" });
-          }
-        });
-      } else {
-        res.send({ msg: "Usuário não registrado!" });
-      }
-    });
+                celular: result[0].celular,
+                endereco: result[0].endereco,
+                isAdmin: result[0].isAdmin,
+                Img: result[0].Img,
+              }),
+            },
+          });
+        } else {
+          res.send({ msg: "Senha incorreta!" });
+        }
+      });
+    } else {
+      res.send({ msg: "Usuário não registrado!" });
+    }
   });
-  
-  module.exports = router;
+});
+
+module.exports = router;
