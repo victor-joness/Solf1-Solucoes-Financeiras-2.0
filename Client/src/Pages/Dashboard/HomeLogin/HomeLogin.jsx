25321f2a90a2bd4d873f96506f8d4746ca19d268
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-04-09 08:15:10 -0300

    Create HomeLogin.jsx

---
+++ Client/src/Pages/Dashboard/HomeLogin/HomeLogin.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./HomeLogin.css";
+import { useNavigate } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+
+//imports das images
+import grafico from "../../../images/GRAFICOS.png";
+import node from "../../../images/NODE-LOGO.png";
+import ufc from "../../../images/UFC-LOGO.png";
+import react from "../../../images/REACT-LOGO.png";
+import Navbar from "../../../Components/Navbar/Navbar";
+
+import NotFound from "../../NotFound/NotFound";
+
+const HomeLogin = () => {
+  const auth = useSelector((state) => {
+    return state.auth;
+  });
+
+  const navigate = useNavigate();
+
+  const redirecionaprapaginalogin = () => {};
+
+  if (auth.token) {
+    return (
+      <div className="container--pagina--inicial-homelogin">
+        <Navbar></Navbar>
+        <div className="container-direita-homelogin">
+          <div className="container-direita-header">
+            <img src={`/upload/${auth.Img}`} alt="Imagem do user" />
+            <h1>{auth.name}</h1>
+          </div>
+
+          <div className="container-direita-center">
+            <div className="infos-top">
+              <div className="infos-top__esquerda">
+                <h1>
+                  FAÇA O CONTROLE DE SEUS GASTOS E GERENCIE SUA VIDA FINANCEIRA
+                </h1>
+              </div>
+
+              <div className="infos-top__direita">
+                <img src={grafico} alt="Uma imagem de graficos" />
+              </div>
+            </div>
+
+            <div className="infos-mid">
+              <div className="infos-mid__esquerda">
+                <h1>ACESSE AS INFORMAÇÕES DE SEU PERFIL</h1>
+              </div>
+
+              <div
+                className="infos-mid__direita"
+                onClick={redirecionaprapaginalogin}
+              >
+                <p>Perfil</p>
+              </div>
+            </div>
+
+            <div className="infos-bot">
+              <div className="infos-bot__container">
+                <img src={node} alt="" />
+              </div>
+
+              <div className="infos-bot__container">
+                <img src={ufc} alt="" />
+              </div>
+
+              <div className="infos-bot__container">
+                <img src={react} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <NotFound></NotFound>;
+  }
+};
+
+export default HomeLogin;
