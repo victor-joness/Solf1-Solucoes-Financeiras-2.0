2195ee3f1bdd2d4d0faff603804b1a544154aaa6
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-04-22 11:04:59 -0300

    Update UserContainer.jsx

--- Client/src/Components/UserContainer/UserContainer.jsx
+++ Client/src/Components/UserContainer/UserContainer.jsx
@@ -2,11 +2,24 @@ import React from "react";
 import "./UserContainer.css";
 
 import { useSelector } from "react-redux";
+import { enderecoFetch } from "../../Features/enderecoSlice";
+import { useDispatch } from "react-redux";
 
 const UserContainer = () => {
   const auth = useSelector((state) => {
     return state.auth;
   });
+
+  const endereco = useSelector((state) => {
+    return state.endereco;
+  });
+
+  const dispatch = useDispatch();
+
+  //talvez melhorar o scripr de reload
+  if (endereco.id === "") {
+    dispatch(enderecoFetch(auth.id));
+  }
 
   return (
     <div className="user-infos-container">
@@ -25,7 +38,7 @@ const UserContainer = () => {
           {`Celular : ${auth.celular}`}
         </p>
         <p className="user-infos-container__pessoais__Endereço">
-          {`Endereço : ${auth.endereco}`}
+          {`Endereço : ${endereco.cidade}, ${endereco.bairro}, ${endereco.numero}`}
         </p>
 
         <p className="user-infos-container__pessoais__Saldo">
