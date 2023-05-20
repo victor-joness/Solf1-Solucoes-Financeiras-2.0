5322b6c1b7e1a3d2dc865b9cf9519fa4a26f7978
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-05-20 08:09:21 -0300

    Create styles.ts

---
+++ Client/src/Pages/Dashboard/Transacoes/components/tableArea/styles.ts
@@ -0,0 +1,63 @@
+import styled from 'styled-components'
+
+export const Container = styled.div<{theme: string}>`
+    width: auto;
+    background-color: ${props => props.theme.status === 'light' ? '#fff' : '#000'};
+    padding: 20px;
+    box-shadow: 0px 0px 5px #ccc;
+    border-radius: 15px;
+    margin-top: 20px;
+    color:  ${props => props.theme.status === 'light' ? '#000' : '#fff'};
+`;
+
+export const Table = styled.table`
+    width: 100%;    
+`;
+
+export const TableHeadColumn = styled.th<{ width?: number}>`
+    padding: 10px 0px;
+    text-align: left;
+    width: ${props => props.width ? `${props.width}px` : 'auto' };
+`;
+
+export const SearchArea = styled.div<{theme: string}>`
+    padding: 20px;
+    display: flex;
+    justify-content: start;
+    box-shadow: 0px 0px 5px #ccc;
+    border-radius: 15px;
+    margin-bottom: 10px;
+
+    input {
+        width: 100%;
+        height: auto;
+        padding: 10px;
+        outline: 0;
+        border: 2px solid #ccc;
+        border-radius: 15px 0px 0px 15px;
+        border-right: 0;
+        background-color: transparent;
+        color: ${props => props.theme.status === 'light' ? '#000' : '#fff'};
+
+        &::placeholder {
+            color: ${props => props.theme.status === 'light' ? '#000' : '#fff'};
+        }
+    }
+
+    select {
+        border: 2px solid #ccc;
+        width: 30%;
+        height: auto;
+        padding: 10px
+        outline: 0;
+        border-radius: 0px 15px 15px 0px;
+        background-color: transparent;
+        color: ${props => props.theme.status === 'light' ? '#000' : '#fff'};
+    };
+
+    .option {
+        background-color: ${props => props.theme.status === 'light' ? '#fff' : '#000'};
+        color: ${props => props.theme.status === 'light' ? '#000' : '#fff'};
+    }
+
+`;
