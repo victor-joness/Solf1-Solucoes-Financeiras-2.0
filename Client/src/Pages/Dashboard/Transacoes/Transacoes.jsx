bce80ba0f0487fa0d0362184ddc7452d7c64227b
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-05-20 08:09:04 -0300

    Update Transacoes.jsx

--- Client/src/Pages/Dashboard/Transacoes/Transacoes.jsx
+++ Client/src/Pages/Dashboard/Transacoes/Transacoes.jsx
@@ -1,147 +1,101 @@
-import React, { useEffect, useState } from "react";
-import "./Transacoes.css";
+import * as c from './app.styles'
+import { TableArea } from './components/tableArea';
+import { InfoArea } from './components/infoArea';
+import { InputArea } from './components/inputArea';
+import { categories, items } from './data/data';
+import { useState, useEffect } from 'react';
+import { getCurrentMonth, FilterListByMonth } from './helpers/dateFilter';
+import { ThemeSwitcher } from './components/themeSwitcher';
+import { useAppSelector } from './redux/hooks/useAppSelector';
+import { useSelector } from 'react-redux';
 
-import Navbar from "../../../Components/Navbar/Navbar";
+const Transacoes = () => {  
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, SetIncome] = useState(0);
+  const [expense, SetExpense] = useState(0);
 
-import { DataGrid } from "@mui/x-data-grid";
-
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import Header from "../../../Components/Header/Header";
-
-import UserContainer from "../../../Components/UserContainer/UserContainer";
-import OptionsContainer from "../../../Components/OptionsContainer/OptionsContainer";
-import NotFound from "../../NotFound/NotFound";
-
-import { updateUser } from "../../../Features/authSlice";
-import { toast } from "react-toastify";
-
-const Transacoes = () => {
-  const auth = useSelector((state) => {
-    return state.auth;
-  });
-
-  const dispatch = useDispatch();
-
-  /* console.log(auth); */
-
-  const navigate = useNavigate();
+  const theme = useAppSelector(state => state.theme);
+  const searchItem = useSelector(state => state.searchItem);
 
   useEffect(() => {
-    if (auth.id) {
-      navigate("/transacoes");
+    setFilteredList(FilterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
+  console.log(searchItem);
+
+  useEffect(() => {
+    let  incomeCount = 0;
+    let expenseCount = 0;
+
+    for (let i in filteredList) {
+      if (searchItem.title === filteredList[i].title && searchItem.category === filteredList[i].category) {
+        if (categories[filteredList[i].category].expense) {
+          expenseCount += filteredList[i].value;
+        } else {
+          incomeCount += filteredList[i].value;
+        } 
+      } else if (searchItem.title === filteredList[i].title) {
+        if (categories[filteredList[i].category].expense) {
+          expenseCount += filteredList[i].value;
+        } else {
+          incomeCount += filteredList[i].value;
+        } 
+      } else if (searchItem.category === filteredList[i].category) {
+        if (categories[filteredList[i].category].expense) {
+          expenseCount += filteredList[i].value;
+        } else {
+          incomeCount += filteredList[i].value;
+        } 
+      } else if (searchItem.category === '' && searchItem.title === '') {
+        if (categories[filteredList[i].category].expense) {
+          expenseCount += filteredList[i].value;
+        } else {
+          incomeCount += filteredList[i].value;
+        }
+      } 
     }
-  }, [auth.id, navigate]);
+    
+    SetIncome(incomeCount);
+    SetExpense(expenseCount);
 
-  const [todastransacoesdousuario, setTodastransacoesdousuario] = useState([
-    {
-      id: "1",
-      valor: "100",
-      data: "15/05/2021",
-      categoria: "teste2",
-      descricao: "teste",
-      tipo: "teste",
-      status: "teste",
-    },
-    {
-        id: "2",
-        valor: "200",
-        data: "16/05/2021",
-        categoria: "teste2",
-        descricao: "teste2",
-        tipo: "teste2",
-        status: "teste2",
-      },
-  ]);
+  }, [filteredList, searchItem.title, searchItem.category]);
 
-  const handleUpdate = () => {};
+  const handleMonthChange = (newMonth) => {
+    setCurrentMonth(newMonth);
+  };
 
-  const handleDelete = () => {};
+  const handleAddItem = (item) => {
+    let newList = [...list];
+    newList.push(item);
+    setList(newList)
+  };
 
-  const columns = [
-    { field: "id", headerName: "ID", width: 100 },
-    { field: "valor", headerName: "Valor", width: 120 },
-    { field: "descricao", headerName: "Descricao", width: 150 },
-    { field: "data", headerName: "Data", width: 150 },
-    { field: "categoria", headerName: "categoria", width: 150 },
-    { field: "tipo", headerName: "Tipo", width: 150 },
-    { field: "status", headerName: "Status", width: 150 },
-    {
-      field: "Ações",
-      headerName: "Ações",
+  const handleDeleteItem = (title) => {
 
-      sortable: false,
-      width: 180,
-      renderCell: (params) => {
-        return (
-          <div className="actions">
-            <button
-              onClick={() => handleDelete(params.row.id)}
-              className="delete"
-            >
-              Delete
-            </button>
-            <button
-              onClick={() => handleUpdate(params.row.id)}
-              className="update"
-            >
-              update
-            </button>
-          </div>
-        );
-      },
-    },
-  ];
+    let newlist = list.filter((item) => {
+      if (item.title !== title)
+        return item;
+    });
 
-  if (auth.token) {
-    return (
-      <div className="container-perfil">
-        <Navbar />
-        <div className="container-direita-perfil">
-          <Header></Header>(
-          <div className="OptionsConta--container">
-            <div className="OptionsConta--container--direita">
-              <div className="OptionsConta--container--direita__mid">
-                <div className="OptionsConta--container--direita--mid__esquerda">
-                  <UserContainer></UserContainer>
-                  <OptionsContainer></OptionsContainer>
-                </div>
-
-                <div className="OptionsConta--container--direita--mid__direita">
-                  <div className="OptionsConta--container--direita--mid__direita--infos">
-                    <h1>Container que vai ter os input de transações</h1>
-                  </div>
-
-                  <div className="container-table-listagem">
-                    <div className="table">
-                      <div
-                        style={{
-                          height: "55vh",
-                          width: "auto",
-                          fontSize: "1.5rem",
-                        }}
-                      >
-                        <DataGrid
-                          rows={todastransacoesdousuario}
-                          columns={columns}
-                          pageSize={10}
-                          rowsPerPageOptions={[10]}
-                          checkboxSelection
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          );
-        </div>
-      </div>
-    );
-  } else {
-    return <NotFound></NotFound>;
+    setList(newlist);
   }
-};
 
+  return (
+    <c.Container theme={theme}>
+      <c.Header theme={theme}>
+        <c.HeaderArea>
+          <c.HeaderText>Gestor de Finanças</c.HeaderText>
+          <ThemeSwitcher />
+        </c.HeaderArea>
+      </c.Header>
+      <c.Body>
+        <InfoArea income={income} expense={expense} onMonthChange={handleMonthChange} currentMonth={currentMonth} />
+        <InputArea onAdd={handleAddItem} />
+        <TableArea deleteItem={handleDeleteItem} list={filteredList} />
+      </c.Body>
+    </c.Container>
+  )
+}
 export default Transacoes;
