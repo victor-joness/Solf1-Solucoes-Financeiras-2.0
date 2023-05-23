34aa89bb51431401270f48e3dfdced1d6de1ef80
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-05-23 10:50:09 -0300

    Update Transacoes.jsx

--- Client/src/Pages/Dashboard/Transacoes/Transacoes.jsx
+++ Client/src/Pages/Dashboard/Transacoes/Transacoes.jsx
@@ -5,7 +5,6 @@ import { InputArea } from "./components/inputArea";
 import { categories, items } from "./data/data";
 import { useState, useEffect } from "react";
 import { getCurrentMonth, FilterListByMonth } from "./helpers/dateFilter";
-import { ThemeSwitcher } from "./components/themeSwitcher";
 import { useAppSelector } from "./redux/hooks/useAppSelector";
 import { useSelector, useDispatch } from "react-redux";
 import Navbar from "../../../Components/Navbar/Navbar";
